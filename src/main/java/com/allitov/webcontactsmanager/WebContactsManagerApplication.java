c6338b95eb1846898e5c5a1034cd4485d0edeb10
allitov <litovkinalexan@gmail.com>
2023-11-09 22:31:19 +0700

    Init commit

---
+++ src/main/java/com/allitov/webcontactsmanager/WebContactsManagerApplication.java
@@ -0,0 +1,13 @@
+package com.allitov.webcontactsmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebContactsManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebContactsManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/allitov/webcontactsmanager/WebContactsManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allitov.webcontactsmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebContactsManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
